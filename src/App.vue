dae2640f2b5edcbaed829bbcbe22ecbd7bd7ae58
JP19-03 <u202014511@upc.edu.pe>
2025-05-20 12:22:44 -0500

    feat(bondValuation): update layout logic to include CorporateBond layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script>
 import EmptyLayoutComponent from "./shared/layouts/empty-layout.component.vue";
+import CorporateBondLayoutComponent from "./bondValuation/layouts/corporate-bond-layout.component.vue";
 
 export default {
   name: "BondTech App",
   components: { EmptyLayoutComponent },
   computed: {
     layout() {
-      return this.$route.meta.layout || 'ParkingManagementLayout' || 'EmptyLayout' || 'AuthLayout';
+      return this.$route.meta.layout || 'CorporateBondLayoutComponent' || 'EmptyLayout';
     }
   },
 }
