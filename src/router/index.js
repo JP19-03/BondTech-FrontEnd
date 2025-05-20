54759c968d1a97371246ffe5154f0ef3e3e119c7
JP19-03 <u202014511@upc.edu.pe>
2025-05-19 23:38:50 -0500

    feat(iam): add sign-in, sign-up components and their routes.

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import {createRouter, createWebHistory} from "vue-router";
+import SignUpComponent from "../iam/components/sign-up.component.vue";
+import SignInComponent from "../iam/components/sign-in.component.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/sign-up',
+            name: 'sign-up',
+            component: SignUpComponent,
+            meta: {title: 'Sign Up', layout: 'AuthenticationLayoutComponent'},
+        },
+        {
+            path: '/sign-in',
+            name: 'sign-in',
+            component: SignInComponent,
+            meta: {title: 'Sign In', layout: 'AuthenticationLayoutComponent'},
+        }
+    ]
+});
+
+export default router;
