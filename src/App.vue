96c2b476a4f0e9db8b7e586dfb9f6bb8752b0505
JP19-03 <u202014511@upc.edu.pe>
2025-05-21 20:25:22 -0500

    feat(bondValuation): add CorporateBondLayoutComponent to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
+<template>
+  <component :is="layout">
+    <router-view/>
+  </component>
+  <pv-toast />
+</template>
+
 <script>
 import EmptyLayoutComponent from "./shared/layouts/empty-layout.component.vue";
 import CorporateBondLayoutComponent from "./bondValuation/layouts/corporate-bond-layout.component.vue";
 
 export default {
   name: "BondTech App",
-  components: { EmptyLayoutComponent },
+  components: { EmptyLayoutComponent, CorporateBondLayoutComponent },
   computed: {
     layout() {
       return this.$route.meta.layout || 'CorporateBondLayoutComponent' || 'EmptyLayout';
@@ -12,12 +19,6 @@ export default {
   },
 }
 </script>
-
-<template>
-  <component :is="layout">
-    <router-view/>
-  </component>
-</template>
 
 <style scoped>
 
